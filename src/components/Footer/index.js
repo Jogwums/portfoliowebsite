28ab96db9a5fcfd3187e404aae4a6f3edba4adfa
Jonathan Ogwumike <jonathanogwumike@gmail.com>
2021-08-04 10:08:45 +0100

    style contributions --Jedo

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ const FooterDetails = () => {
     <>
       <Navbar class="py-5 bg-black">
             <div class="container px-5 bg-black">
-              <p class="m-0 text-center text-white small">Copyright &copy; Jonathan Ogwumike 2021</p></div>
+              <p class="m-0 text-center small">Copyright &copy; Jonathan Ogwumike 2021</p></div>
         </Navbar>
     </>
   )
