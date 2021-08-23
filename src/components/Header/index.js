6de0d20817298204311c87343f070c09ed5d54e6
Jonathan Ogwumike <jonathanogwumike@gmail.com>
2021-08-23 17:45:13 +0100

    working on header comp

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,17 +11,13 @@ const HeaderDetails = () => {
     <Jumbotron className="p-5">
       <Header className="masthead text-black">
             <div className="masthead-content md-ml-5 md-pl-5">
-                <Container className="px-5">
+                <Container className="container-fluid md-px-5">
                     <h1 className="masthead-heading mb-0">Jonathan Ogwumike</h1>
                     <br />
                     <h6 className="masthead-subheading mb-0">Front-End Systems Developer</h6>
                     <a className="btn btn-warning btn-xl rounded-pill mt-5" href="#scroll">Projects</a>
                 </Container>
             </div>
-            <div className="bg-circle-1 bg-circle"></div>
-            <div className="bg-circle-2 bg-circle"></div>
-            <div className="bg-circle-3 bg-circle"></div>
-            <div className="bg-circle-4 bg-circle"></div>
         </Header>
     </Jumbotron>
   )
