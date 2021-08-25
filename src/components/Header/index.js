cb2a1fd3c39a3e190f6a91f9b6befc89c7ec0399
Jonathan Ogwumike <jonathanogwumike@gmail.com>
2021-08-25 16:12:06 +0100

    new fonts poppins and roboto

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import Header from 'react-bootstrap/ModalHeader';
-import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
-// import Form from 'react-bootstrap/Form';
-// import Button from 'react-bootstrap/Button';
-// import FormControl from 'react-bootstrap/FormControl';
+
 
 const HeaderDetails = ({type, text}) => {
     return (
-    <Jumbotron className="p-5">
+    <div className="p-5 bg-primary">
       <Header className="masthead text-black">
             <div className="masthead-content md-ml-5 md-pl-5">
                 <Container className="container-fluid md-px-5">
-                    <h1 className="masthead-heading mb-0">Jonathan Ogwumike</h1>
+                    <h1 className="masthead-heading mb-0 text-white">Jonathan Ogwumike</h1>
                     <br />
                     <h6 className="masthead-subheading mb-0">{text}</h6>
                     <a className="btn btn-warning btn-xl rounded-pill mt-5" href="#scroll">{type}</a>
                 </Container>
             </div>
         </Header>
-    </Jumbotron>
+    </div>
   )
 }
 
