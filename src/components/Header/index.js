addc0bf2102859b8b34226613dfe342839ac2e7d
Jonathan Ogwumike <jonathanogwumike@gmail.com>
2021-09-14 14:24:15 +0100

    centered content on mobile

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,7 +9,7 @@ const HeaderDetails = ({type, text}) => {
       <Header className="masthead text-black">
             <div className="masthead-content md-ml-5 md-pl-5">
                 <Container className="container-fluid md-px-5">
-                    <h1 className="masthead-heading mb-0 text-white">Jonathan Ogwumike</h1>
+                    <h1 className="masthead-heading mb-0 text-white fst-italic">Jonathan Ogwumike</h1>
                     <br />
                     <h6 className="masthead-subheading mb-0">{text}</h6>
                     <a className="btn btn-warning btn-xl rounded-pill mt-5" href="#scroll">{type}</a>
