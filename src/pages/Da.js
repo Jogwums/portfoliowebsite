1ca1ed12857aa75a5288a4bd0cbcbd93f5465c40
Jonathan Ogwumike <jonathanogwumike@gmail.com>
2022-05-26 16:05:25 +0100

    added a simplified da portfolio structure

--- src/pages/Da.js
+++ src/pages/Da.js
@@ -7,6 +7,7 @@ import Card from '../components/Card'
 // images 
 import image1 from '../assets/img/pbiprojects/pbi1.JPG'
 import image2 from '../assets/img/pbiprojects/pbi2.JPG'
+import image3 from '../assets/img/pbiprojects/pbi3.JPG'
 
 const Da = ({type, text}) => {
 
@@ -15,30 +16,18 @@ const Da = ({type, text}) => {
     return (
         <>
         <Navigation />
-        <TitleDetails title="PowerBi Report" text="Report on World Population. Data Source:'Worldometer (www.Worldometers.info)'"/>
-        <div className="pbi-dashboards">
-            {/* eslint-disable-next-line */}
-            <iframe 
-                width="1140" 
-                height="541.25" 
-                src="https://app.powerbi.com/reportEmbed?reportId=283c0606-8fa6-4cb4-b66d-236157b1a667&autoAuth=true&ctid=e28e2cde-f37f-45db-bace-a4b1dd277623&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWFmcmljYS1ub3J0aC1hLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" 
-                frameborder="0" 
-                allowFullScreen="true">
-
-            </iframe>
-        </div>
-        <TitleDetails title="PBI Report Link" text="Report on World Covid-19 Outbreak. Data Source:'Worldometer (www.Worldometers.info)'"/>
+        <TitleDetails title="Power BI Reports" text=""/>
         <div className="card-deck">
         <Card image={image1} 
-                        text={"Click the Image"} 
+                        text={"Report on World Covid-19 Outbreak"} 
                         text2={"You will be redirected to the report"}
                         link={"https://app.powerbi.com/reportEmbed?reportId=131418d5-442b-488a-a088-3913e1255a84&autoAuth=true&ctid=e28e2cde-f37f-45db-bace-a4b1dd277623&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWFmcmljYS1ub3J0aC1hLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D"} />
 
         <Card
-            image={image2}
-            text={"Plane Crashes Worldwide"}
+            image={image3}
+            text={"World Population Report"}
             text2={"You will be redirected to the report"}
-            link={"https://app.powerbi.com/reportEmbed?reportId=45bbd2da-2fef-4fc2-9c9e-f6af8da5328e&autoAuth=true&ctid=e28e2cde-f37f-45db-bace-a4b1dd277623&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWFmcmljYS1ub3J0aC1hLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D"}
+            link={"https://app.powerbi.com/reportEmbed?reportId=283c0606-8fa6-4cb4-b66d-236157b1a667&autoAuth=true&ctid=e28e2cde-f37f-45db-bace-a4b1dd277623&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWFmcmljYS1ub3J0aC1hLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D"}
         />
         <Card
             image={image2}
