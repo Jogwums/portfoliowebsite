8c633733baead6faadbe8eb3b291b56b429ff0f0
Jonathan Ogwumike <jonathanogwumike@gmail.com>
2022-06-12 00:11:10 +0100

    home page styles

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,20 +10,20 @@ import {FaLinkedin, FaGithub, FaKaggle } from 'react-icons/fa'
 const About = () => {
   return (
     <>
-    <section className="jumbotron my-0 py-0 bg-primary" id="parallax">
+  <section className="jumbotron my-0 py-0 bg-primary" id="parallax">
   <div className="container">
     <div className="row">
-      <div className="col-lg-11 mx-auto">
+      <div className="col-lg-9 mx-auto">
         <h1 className="text-white font-tertiary">
         <br /> Solutions Expert | Quick Links
         {/* <br/> Proper Prior Planning Prevents Poor Performance<br/> */}
         </h1>
       </div>
     </div>
-  </div>
+  
   
   {/* <!-- social icon --> */}
-  <div className="socials">
+  <div className="socials py-2">
   <ul className="list-unstyled mt-3 position-relative zindex-1">
     {/* <li className='mb-3'><a className='text-white' href="/"><FaTwitter size={`2em`} /></a></li> */}
     {/* <li className='mb-3'><a className='text-white' href="/"><FaFacebookF size={`2em`} /></a></li> */}
@@ -35,23 +35,27 @@ const About = () => {
   </ul>
   </div>
   {/* <!-- /social icon --> */}
+  </div>
 </section>
-<section className="section pt-5">
+<section className="section py-5">
   <div className="container">
     <div className="row">
-      <div className="col-md-8">
-        <p>Data and business intelligence analyst. Interested in ml, web development and cloud 
-          computing commodo consequat.</p>
-        <p>Experienced trainer and developer, working with SQL, Microsoft Excel, Power BI, Python, JavaScript, ReactJS, 
-          SPSS, SigmaPlot, Grapher12, Streamlit, CSS, HTML, Bootstrap, Tailwind and other technologies 
-          for data analysis, web development and machine learning.</p>
-        <div >
-          <blockquote>
+      <div className="col-md-8 ">
+        <div className='lead'>
+          <p>Data and business intelligence analyst. Interested in ml, web development and cloud 
+            computing commodo consequat.</p>
+          <p>Experienced trainer and developer, working with SQL, Microsoft Excel, Power BI, Python, JavaScript, ReactJS, 
+            SPSS, SigmaPlot, Grapher12, Streamlit, CSS, HTML, Bootstrap, Tailwind and other technologies 
+            for data analysis, web development and machine learning.</p>
+        </div>
+        <div>
+          <blockquote className='blockquote'>
           A Jack of All Trades is a Master of None. 
           But Oftentimes better than a Master of One
           </blockquote>
         </div>
       </div>
+      
       <div className="col-md-4 text-center drag-lg-top">
         <div className="shadow-down mb-4">
           <img src={author} alt="author" className="img-fluid w-100 rounded-lg border-thick border-white" />
